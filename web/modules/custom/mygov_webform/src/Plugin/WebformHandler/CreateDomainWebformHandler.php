99d84d11be64636fa142325ed78a0b0e8b3238f7
Alex Ho <alexho1@asu.edu>
2021-03-07 13:15:56 -0500

    add a webform handler plugin
    @see https://industrialagency.ca/blog/create-custom-webform-handler-drupal-8

---
+++ web/modules/custom/mygov_webform/src/Plugin/WebformHandler/CreateDomainWebformHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\mygov_webform\Plugin\WebformHandler;
+
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionInterface;
+
+/**
+ * Create a new node entity from a webform submission.
+ *
+ * @WebformHandler(
+ *   id = "Create a domain",
+ *   label = @Translation("Create a domain"),
+ *   category = @Translation("Action"),
+ *   description = @Translation("Creates a new node from Webform Submissions."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+class CreateDomainWebformHandler extends WebformHandlerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  // Function to be fired after submitting the Webform.
+  public function postSave(WebformSubmissionInterface $webform_submission, $update = TRUE) {
+    // Get an array of the values from the submission.
+    $values = $webform_submission->getData();
+  }
+}
+
