5b7e3986959858876571b0c4fa5356a0a887e4a7
Alex Ho <alexho1@asu.edu>
2021-04-08 21:57:34 -0400

    map state abbreviations

--- web/modules/custom/mygov_webform/src/Plugin/WebformHandler/CreateDomainWebformHandler.php
+++ web/modules/custom/mygov_webform/src/Plugin/WebformHandler/CreateDomainWebformHandler.php
@@ -47,7 +47,7 @@ class CreateDomainWebformHandler extends WebformHandlerBase {
         $list['county'] = $term->label();
       }
       else {
-        $list['state'] = $term->label();
+        $list['state'] = $this->convertState($term->label());
       }
     }
 
@@ -121,6 +121,82 @@ class CreateDomainWebformHandler extends WebformHandlerBase {
       $text = new TranslatableMarkup("Your site has been created! Visit the page here - @link", ["@link" => $link]);
       $messenger->addMessage($text, $messenger::TYPE_STATUS);
     }
+  }
+
+  private function convertState($name) {
+    $states = array(
+      array('name'=>'Alabama', 'abbr'=>'AL'),
+      array('name'=>'Alaska', 'abbr'=>'AK'),
+      array('name'=>'Arizona', 'abbr'=>'AZ'),
+      array('name'=>'Arkansas', 'abbr'=>'AR'),
+      array('name'=>'California', 'abbr'=>'CA'),
+      array('name'=>'Colorado', 'abbr'=>'CO'),
+      array('name'=>'Connecticut', 'abbr'=>'CT'),
+      array('name'=>'Delaware', 'abbr'=>'DE'),
+      array('name'=>'Florida', 'abbr'=>'FL'),
+      array('name'=>'Georgia', 'abbr'=>'GA'),
+      array('name'=>'Hawaii', 'abbr'=>'HI'),
+      array('name'=>'Idaho', 'abbr'=>'ID'),
+      array('name'=>'Illinois', 'abbr'=>'IL'),
+      array('name'=>'Indiana', 'abbr'=>'IN'),
+      array('name'=>'Iowa', 'abbr'=>'IA'),
+      array('name'=>'Kansas', 'abbr'=>'KS'),
+      array('name'=>'Kentucky', 'abbr'=>'KY'),
+      array('name'=>'Louisiana', 'abbr'=>'LA'),
+      array('name'=>'Maine', 'abbr'=>'ME'),
+      array('name'=>'Maryland', 'abbr'=>'MD'),
+      array('name'=>'Massachusetts', 'abbr'=>'MA'),
+      array('name'=>'Michigan', 'abbr'=>'MI'),
+      array('name'=>'Minnesota', 'abbr'=>'MN'),
+      array('name'=>'Mississippi', 'abbr'=>'MS'),
+      array('name'=>'Missouri', 'abbr'=>'MO'),
+      array('name'=>'Montana', 'abbr'=>'MT'),
+      array('name'=>'Nebraska', 'abbr'=>'NE'),
+      array('name'=>'Nevada', 'abbr'=>'NV'),
+      array('name'=>'New Hampshire', 'abbr'=>'NH'),
+      array('name'=>'New Jersey', 'abbr'=>'NJ'),
+      array('name'=>'New Mexico', 'abbr'=>'NM'),
+      array('name'=>'New York', 'abbr'=>'NY'),
+      array('name'=>'North Carolina', 'abbr'=>'NC'),
+      array('name'=>'North Dakota', 'abbr'=>'ND'),
+      array('name'=>'Ohio', 'abbr'=>'OH'),
+      array('name'=>'Oklahoma', 'abbr'=>'OK'),
+      array('name'=>'Oregon', 'abbr'=>'OR'),
+      array('name'=>'Pennsylvania', 'abbr'=>'PA'),
+      array('name'=>'Rhode Island', 'abbr'=>'RI'),
+      array('name'=>'South Carolina', 'abbr'=>'SC'),
+      array('name'=>'South Dakota', 'abbr'=>'SD'),
+      array('name'=>'Tennessee', 'abbr'=>'TN'),
+      array('name'=>'Texas', 'abbr'=>'TX'),
+      array('name'=>'Utah', 'abbr'=>'UT'),
+      array('name'=>'Vermont', 'abbr'=>'VT'),
+      array('name'=>'Virginia', 'abbr'=>'VA'),
+      array('name'=>'Washington', 'abbr'=>'WA'),
+      array('name'=>'West Virginia', 'abbr'=>'WV'),
+      array('name'=>'Wisconsin', 'abbr'=>'WI'),
+      array('name'=>'Wyoming', 'abbr'=>'WY'),
+    );
+
+    $return = false;
+    $strlen = strlen($name);
+
+    foreach ($states as $state) :
+      if ($strlen < 2) {
+        return false;
+      } else if ($strlen == 2) {
+        if (strtolower($state['abbr']) == strtolower($name)) {
+          $return = $state['name'];
+          break;
+        }
+      } else {
+        if (strtolower($state['name']) == strtolower($name)) {
+          $return = strtoupper($state['abbr']);
+          break;
+        }
+      }
+    endforeach;
+
+    return $return;
   }
 }
 
